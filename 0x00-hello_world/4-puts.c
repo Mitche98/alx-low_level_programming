5c1e63cc6f83b34379686ee7b70249946b3371a9
Mitche98 <jerryomokaro62@gmail.com>
2023-03-16 22:53:11 +0000

    Ignorant

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/** 
+ * main - initializes function
+ *
+ * return(0) - terminates function
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
