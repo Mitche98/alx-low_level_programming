0a22a15975ab4eea48865bddaab15a2aad7b5c49
Mitche98 <jerryomokaro62@gmail.com>
2023-03-18 17:55:25 +0000

    Positive Outcome

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  *main -Prints the last digit of a randomly generated number 
@@ -9,8 +10,9 @@
 int main(void)
 {
 	int n;
-	srand(time(0));
 
+
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;	
 	if (n > 0)
 		printf("%d is positive\n", n);
