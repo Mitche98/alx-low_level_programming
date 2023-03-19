57ff1e3d545baa814c90fa33c509dcd8ec6a508a
Mitche98 <jerryomokaro62@gmail.com>
2023-03-19 23:32:54 +0000

    Maybe This

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main-Entry Point
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 10; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
