b1f34152a26a8cbd414878b6f15cfa4aeba1b7f4
Mitche98 <jerryomokaro62@gmail.com>
2023-03-19 18:39:52 +0000

    Have a nice day

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main-Entry Point
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers <= 9; numbers++)
+
+		putchar(numbers);
+
+	putchar('\n');
+
+	return (0);
+}
