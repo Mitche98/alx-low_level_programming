a3206560ed404de39d7e4076dcbcba39cfc5e2de
Mitche98 <jerryomokaro62@gmail.com>
2023-03-19 17:15:46 +0000

    I could not care less anymore

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,8 +10,13 @@
 int main(void)
 {
 	char letter;
-	for (letter = 'a'; letter <= 'z'; letter != 'q', 'e')
-		putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+	}
+
 	putchar('\n');
+	
 	return (0);
 }
