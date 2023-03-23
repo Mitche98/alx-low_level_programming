2151b9f705270d1c65554ddfa311750689a29cf1
Mitche98 <jerryomokaro62@gmail.com>
2023-03-23 11:16:34 +0000

    Positive/Negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main-Tests function that prints positive or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negeative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_HEADER_
+#define MAIN_HEADER_
+int main(void);
+
+#endif /* _MAIN_H_ */
